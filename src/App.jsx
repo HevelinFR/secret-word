0452dad3c1831a2eedb89faffbdb20d0d29afe97
Hevelin de Jesus Freitas <hevelinfreitas21@gmail.com>
2024-08-12 21:04:57 -0300

    :sparkles: feat: consuming game states

--- src/App.jsx
+++ src/App.jsx
@@ -25,53 +25,75 @@ const stages = [
 ];
 
 function App() {
-    const [gameStage, setGameStage] = useState(stages[0].name)
-    const [ words] = useState(wordsList);
+    const [gameStage, setGameStage] = useState(stages[0].name);
+    const [words] = useState(wordsList);
 
     const [pickedWord, setPickedWord] = useState("");
     const [pickedCategory, setPickedCategory] = useState("");
-    const [letters, setLetters] = useState([])
+    const [letters, setLetters] = useState([]);
 
-    const pickWordAndCategory = () =>{
+    const [guessedletters, setGuesseLetters] = useState([]);
+    const [wrongLetters, setWrongLetters] = useState([]);
+    const [guesses, setGuesses] = useState(3);
+    const [score, setScore] = useState(0);
+
+    const pickWordAndCategory = () => {
         const categories = Object.keys(words);
-        const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
-        const word = words[category][Math.floor(Math.random() * words[category].length)]
+        const category =
+            categories[
+                Math.floor(Math.random() * Object.keys(categories).length)
+            ];
+        const word =
+            words[category][Math.floor(Math.random() * words[category].length)];
 
-       return {word, category}
-    }
+        return { word, category };
+    };
     //start the secret word game
     const startGame = () => {
         //pick word and pick category
-        const {word, category} = pickWordAndCategory();
+        const { word, category } = pickWordAndCategory();
 
         //create array of letters
         let wordLetters = word.split("");
         wordLetters = wordLetters.map((l) => l.toLowerCase());
 
         //fill states
-        setPickedCategory(category);
         setPickedWord(word);
+        setPickedCategory(category);
         setLetters(wordLetters);
 
-        setGameStage(stages[1].name)
-    }
+        setGameStage(stages[1].name);
+    };
 
     //process the letter input
     const verifyLetter = () => {
-        setGameStage(stages[2].name)
-    }
+        setGameStage(stages[2].name);
+    };
 
     //restarts the game
-    const retry = () =>{
-        setGameStage(stages[0].name)
-    }
+    const retry = () => {
+        setGameStage(stages[0].name);
+    };
 
     return (
         <>
             <div className="App">
-                {gameStage === 'start' && <StartScreen startGame={startGame}></StartScreen>}
-                {gameStage === 'game' && <Game verifyLetter={verifyLetter}></Game>}
-                {gameStage === 'end' && <GameOver retry={retry}></GameOver>}
+                {gameStage === "start" && (
+                    <StartScreen startGame={startGame}></StartScreen>
+                )}
+                {gameStage === "game" && (
+                    <Game
+                        verifyLetter={verifyLetter}
+                        pickedWord={pickedWord}
+                        pickedCategory={pickedCategory}
+                        letters={letters}
+                        guessedletters={guessedletters}
+                        wrongLetters={wrongLetters}
+                        guesses={guesses}
+                        score={score}
+                    ></Game>
+                )}
+                {gameStage === "end" && <GameOver retry={retry}></GameOver>}
             </div>
         </>
     );
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,33 +1,50 @@
-import "./Game.css"
+import "./Game.css";
 
-export const Game = ({verifyLetter}) => {
-  return (
-    <div className="game">
-        <p className="points">
-          <span>Pontuação: 000</span>
-        </p>
-        <h1>Adivinhe a palavra</h1>
-        <h3 className="tip">
-          Dica sobe a palavra: <span>Dica...</span>
-        </h3>
-        <div className="wordContainer">
-          <span className="letter">A</span>
-          <span className="blankSquare"></span>
-          <span className="blankSquare"></span>
-          <span className="blankSquare"></span>
+export const Game = ({
+    verifyLetter,
+    pickedWord,
+    pickedCategory,
+    letters,
+    guessedletters,
+    wrongLetters,
+    guesses,
+    score,
+}) => {
+    return (
+        <div className="game">
+            <p className="points">
+                <span>Pontuação: {score}</span>
+            </p>
+            <h1>Adivinhe a palavra</h1>
+            <h3 className="tip">
+                Dica sobe a palavra: <span>{pickedCategory}</span>
+            </h3>
+            <p>Você ainda tem {guesses} tentativas</p>
+            <div className="wordContainer">
+              {letters.map((letter, i) => (
+                guessedletters.includes(letter) ? (
+                  <span className="letter" key={i}>{letter}</span>
+                ) : (
+                  <span className="blankSquare" key={i}></span>
+                )
+              ))
+              }
+            </div>
+            <div className="letterContainer">
+                <p>Tente adivinhar uma letra da palavra:</p>
+                <form>
+                    <input type="text" name="letter" maxLength={1} required />
+                    <button>Jogar!</button>
+                </form>
+            </div>
+            <div className="wrongLettersContainer">
+                <p>Letras já utilizadas:</p>
+
+                {wrongLetters.map((letter, i) => (
+                  <span key={i}>{letter}, </span>
+                ))
+              }
+            </div>
         </div>
-        <div className="letterContainer">
-          <p>Tente adivinhar uma letra da palavra:</p>
-          <form>
-            <input type="text" name="letter" maxLength={1} required />
-            <button>Jogar!</button>
-          </form>
-        </div>
-        <div className="wrongLettersContainer">
-            <p>Letras já utilizadas:</p>
-            <span>a,</span>
-            <span>b,</span>
-        </div>
-    </div>
-  )
-}
+    );
+};
